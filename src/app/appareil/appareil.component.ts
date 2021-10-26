e3456689b2361d8b30264ee3f9867de87605b08a
TRAFANOHARANTSOA <trafanoharantsoa@gmail.com>
2021-10-26 13:04:43 +0200

    string interpolation- method getSatus

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -8,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class AppareilComponent implements OnInit {
 
   appareilName = "Machine à laver";
+  appareilStatus = "éteint";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getStatus(){
+    return this.appareilStatus;
   }
 
 }
